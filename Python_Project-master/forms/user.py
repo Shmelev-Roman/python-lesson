8b959a30eb95fea2a6a576b8c3d053b17ebc9925
Roman Shmelev <shmelyov.roma@inbox.ru>
2021-04-25 12:28:51 +0300

    AAAA

---
+++ Python_Project-master/forms/user.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, BooleanField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    about = TextAreaField("Немного о себе")
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
